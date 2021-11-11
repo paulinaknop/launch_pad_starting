f0c7867b7ec0478ff4fb83535c8a8a08085a0334
paulinaeknop@gmail.com <5!Coo8kQi>
2021-11-11 20:51:30 +0530

    Initial project for starting.

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+
+import 'package:google_fonts/google_fonts.dart';
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
