0740fea43c1e57e036f5d63b2ed8593b7922088c
Paulina Knop <paulinaeknop@gmail.com>
2022-07-29 15:52:25 +0530

    removed desktop.ini && Updated packages.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
-
+import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:audioplayers/audioplayers.dart';
-import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
